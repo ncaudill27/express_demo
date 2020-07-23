8fa71d4faee3a19c6df4558041dda17d35c01cfb
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-23 15:12:05 -0400

    Connect dotenv and add environment variables for spotify api

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 const helloWorld = (req, res) => res.send('Hello World!');
-app.get( '/', helloWorld);
+app.get('/', helloWorld);
 
+app.get('/user', (req, res) => {
+  console.log(process.env.REDIRECT_URI);
+  res.send(process.env.REDIRECT_URI);
+} );
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
