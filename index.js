c2b920489ba6b83d17e4ed328340096f85286f15
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-25 10:52:04 -0400

    Begin setting up to add state parameter to authorization request

--- index.js
+++ index.js
@@ -15,11 +15,19 @@ app.get('/user', (req, res) => {
 
 app.get('/authorize', function(req, res) {
   var scopes = 'user-read-private user-read-email';
+  // cookie needs to be set up
+  // req.session.state
   res.redirect('https://accounts.spotify.com/authorize' +
     '?response_type=code' +
     '&client_id=' + process.env.SPOTIFY_ID +
     (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
     '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URI));
-  });
+});
+
+app.get('/callback', function(req, res) {
+  const state = req.query.state;
+  console.log(state);
+});
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
