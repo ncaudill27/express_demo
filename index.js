a84a998750278315c104442da48584957dd76c71
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-25 21:30:08 -0400

    Extract fetch calls in callback into a function and begin working on cookie

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('trust proxy', 1);
+
 app.use(cookieSession({
   name: 'session',
   keys: [process.env.KEY_1, process.env.KEY_2],
   cookie: {
     // secure: true,
     httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
 app.disable('x-powered-by');
+
 
 app.get('/', (req, res) => {
 
@@ -24,9 +28,12 @@ app.get('/', (req, res) => {
 
   // set state
   req.session.state = id;
-
   console.log('New session state set: ', id); //! DELETE IN PRODUCTION
-  res.send('Hello World!')
+
+  console.log('Cookie: ', req.session);
+  console.log('Username in cookie: ', req.session.username);
+  if (req.session.username) res.send(`Hello ${req.session.username}`)
+  else res.send('Hello World!');
 });
 
 //? useless route?
@@ -35,8 +42,9 @@ app.get('/user', (req, res) => {
   res.send(process.env.REDIRECT_URI);
 } );
 
+
 app.get('/authorize', function(req, res) {
-  const scopes = 'user-read-private user-read-email';
+  const scopes = 'user-read-private user-modify-playback-state playlist-read-private';
   console.log('Sending state: ', req.session.state); //! DELETE IN PRODUCTION
   res.redirect('https://accounts.spotify.com/authorize' +
     '?response_type=code' +
@@ -47,20 +55,29 @@ app.get('/authorize', function(req, res) {
     '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URI));
 });
 
+
 app.get('/callback', function(req, res) {
   const state = req.query.state;
   const err = decodeURIComponent(req.query.error); // failed parameter
-  const code = decodeURIComponent(req.query.code); // success parameter
-  const encodedAuthorization = new Buffer.from(process.env.SPOTIFY_ID + ':' + process.env.SPOTIFY_SECRET).toString('base64');
-  // const  = buffer.toString('base64');
-  console.log('Authorization code: ', code);
-  console.log('Encoded authorization: ', encodedAuthorization);
 
   // deny any responses with altered state parameter
   if (state !== req.session.state) res.end('ACCESS DENIED!!!'); //? secure way to handle code parameter
 
   // show error message if any
   if (err) res.end(err);
+
+  authorizeUser(req, res);
+  
+
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+function authorizeUser(req, res) {
+  const code = req.query.code; // success parameter
+  const encodedAuthorization = new Buffer.from(process.env.SPOTIFY_ID + ':' + process.env.SPOTIFY_SECRET).toString('base64');
+  console.log('Authorization code: ', code); //! DELETE IN PRODUCTION
+  console.log('Encoded authorization: ', encodedAuthorization); //! DELETE IN PRODUCTION
 
   fetch('https://accounts.spotify.com/api/token' + 
     '?grant_type=authorization_code' +
@@ -70,29 +87,34 @@ app.get('/callback', function(req, res) {
       'method': 'POST',
       'headers': {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${encodedAuthorization}`
+        'Authorization': `Basic ${encodedAuthorization}`,
+        'Accept': 'application/json'
       }
     }
   )
   .then( res => res.json() )
   .then( data => {
-    console.log("Data: ", data)
+    console.log("Data: ", data) //! DELETE IN PRODUCTION
     // return error message
     if (data.error) res.end(data.error_description)
 
     const { access_token, token_type, expires_in, refresh_token, scope } = data;
     const tokenAuthorizationHeader = token_type + ' ' + access_token;
+    req.session.refreshToken = refresh_token;
+
     console.log('Authorization Header: ', tokenAuthorizationHeader);
     return fetch('https://api.spotify.com/v1/me', {
       'method': 'GET',
       'headers': {
-        'Authorization': tokenAuthorizationHeader
+        'Authorization': tokenAuthorizationHeader,
+        'Accept': 'application/json'
       }
     })
     .then( res => res.json() )
-    .then( data => console.log(data) )
+    .then( data => {
+      console.log(data) //! DELETE IN PRODUCTION
+      req.session.username = data.display_name;
+      return { username: data.display_name, refreshToken: refresh_token }
+    } )
   } );
-});
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+}
